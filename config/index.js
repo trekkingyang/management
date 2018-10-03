93514e27730ac95cb1cd8e5c6170163f1f9e159e
yangyang6 <yangyang6@vipkid.com.cn>
2018-10-03 17:51:04 +0800

    修改编译路径

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
     proxyTable: {
       '/getList': {  //使用"/api"来代替"http://f.apiplus.c"
         target: 'http://localhost:3000', //源地址
